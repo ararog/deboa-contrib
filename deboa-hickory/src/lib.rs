691057bfcaa0331366010190be388efabe1b2648
Rogério Araújo <rogerio.araujo@gmail.com>
2026-08-03 16:18:03 -0300

    chore: updated deboa to latest version

--- deboa-hickory/src/lib.rs
+++ deboa-hickory/src/lib.rs
@@ -1,46 +1,57 @@
 use deboa::{
     dns::{DnsResolver, DnsResolverFuture},
-    errors::{
-        DeboaError::{self, Dns},
-        DnsError,
-    },
+    errors::{DeboaError::Dns, DnsError},
+    Result,
 };
 use hickory_resolver::{
-    config::ResolverConfig, net::runtime::TokioRuntimeProvider, Hosts, Resolver,
+    config::{ResolverConfig, GOOGLE},
+    net::runtime::TokioRuntimeProvider,
+    ResolverBuilder, TokioResolver,
 };
+use once_cell::sync::Lazy;
 use rand::seq::SliceRandom;
+use std::{net::IpAddr, sync::Arc};
+
+static GLOBAL_RESOLVER: Lazy<Arc<TokioResolver>> = Lazy::new(|| {
+    let resolver = TokioResolver::builder_with_config(
+        ResolverConfig::udp_and_tcp(&GOOGLE),
+        TokioRuntimeProvider::default(),
+    )
+    .build()
+    .expect("Failed to create Hickory resolver");
+    Arc::new(resolver)
+});
 
 #[derive(Default)]
 pub struct HickoryDnsResolver;
 
+impl HickoryDnsResolver {
+    pub fn builder() -> Result<ResolverBuilder<TokioRuntimeProvider>> {
+        TokioResolver::builder_tokio()
+            .map_err(|e| Dns(DnsError::Resolver { message: e.to_string() }))
+    }
+}
+
 impl DnsResolver for HickoryDnsResolver {
     fn resolve(&self, host: String, _port: u16) -> DnsResolverFuture {
+        let resolver = GLOBAL_RESOLVER.clone();
         let future = async move {
-            let mut resolver = Resolver::builder_with_config(
-                ResolverConfig::default(),
-                TokioRuntimeProvider::default(),
-            )
-            .build()
-            .map_err(|e| {
-                DeboaError::Dns(DnsError::Resolve { host: host.clone(), message: e.to_string() })
-            })?;
+            let mut ips: Vec<IpAddr> = {
+                let addrs = resolver
+                    .lookup_ip(&host)
+                    .await;
+                if let Err(e) = addrs {
+                    return Err(Dns(DnsError::Resolve { host, message: e.to_string() }));
+                }
 
-            if let Ok(hosts) = Hosts::from_system() {
-                resolver.set_hosts(hosts.into());
-            }
-
-            let addrs = resolver
-                .lookup_ip(&host)
-                .await;
-            if let Err(e) = addrs {
-                return Err(Dns(DnsError::Resolve { host, message: e.to_string() }));
+                addrs
+                    .unwrap()
+                    .iter()
+                    .collect()
             };
 
-            let mut ips = addrs
-                .unwrap()
-                .iter()
-                .collect::<Vec<_>>();
             ips.shuffle(&mut rand::rng());
+
             Ok(ips)
         };
 
